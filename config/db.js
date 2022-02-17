68e5fbe7362a9ecd709fc0dbc643b7153a506528
NogyJ0D <81924427+NogyJ0D@users.noreply.github.com>
2022-02-17 19:54:00 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,7 +3,10 @@ const config = require('./index')
 
 const connection = async () => {
   const conn = await mongoose.connect(
-    `mongodb+srv://${config.dbUsername}:${config.dbPassword}@${config.dbHost}/${config.dbName}`
+    `mongodb+srv://${config.dbUsername}:${config.dbPassword}@${config.dbHost}/${config.dbName}
+     mongodb://${config.dbUsername}:${config.dbPassword}@backendnodetzul-shard-00-00.k9imb.mongodb.net:27017,backendnodetzul-shard-00-01.k9imb.mongodb.net:27017,backendnodetzul-shard-00-02.k9imb.mongodb.net:27017/${config.dbName}?ssl=true&replicaSet=atlas-a95nws-shard-0&authSource=admin&retryWrites=true&w=majority
+mongodb://${config.dbUsername}:${config.dbPassword}@backendnodetzul-shard-00-00.k9imb.mongodb.net:27017,backendnodetzul-shard-00-01.k9imb.mongodb.net:27017,backendnodetzul-shard-00-02.k9imb.mongodb.net:27017/${config.dbName}?ssl=true&replicaSet=atlas-a95nws-shard-0&authSource=admin&retryWrites=true&w=majority
+`
   )
   console.log(`~MongoDB connected in ${config.dbName} base~`)
 }
