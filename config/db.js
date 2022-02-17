efb02bdc8ac13586fa42d00f19f69b36b0344d3f
NogyJ0D <81924427+NogyJ0D@users.noreply.github.com>
2022-02-17 20:06:21 -0300

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,8 +3,7 @@ const config = require('./index')
 
 const connection = async () => {
   const conn = await mongoose.connect(
-    `mongodb://${config.dbUsername}:${config.dbPassword}@backendnodetzul-shard-00-00.k9imb.mongodb.net:27017,backendnodetzul-shard-00-01.k9imb.mongodb.net:27017,backendnodetzul-shard-00-02.k9imb.mongodb.net:27017/${config.dbName}?ssl=true&replicaSet=atlas-a95nws-shard-0&authSource=admin&retryWrites=true&w=majority
-mongodb://${config.dbUsername}:${config.dbPassword}@backendnodetzul-shard-00-00.k9imb.mongodb.net:27017,backendnodetzul-shard-00-01.k9imb.mongodb.net:27017,backendnodetzul-shard-00-02.k9imb.mongodb.net:27017/${config.dbName}?ssl=true&replicaSet=atlas-a95nws-shard-0&authSource=admin&retryWrites=true&w=majority`
+    `mongodb+srv://${config.dbUsername}:${config.dbPassword}@${config.dbHost}/${config.dbName}`
   )
   console.log(`~MongoDB connected~`)
 }
